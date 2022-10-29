d7062c4b01e92b7d26fab99e0fc3036836681d63
Avi Rana <itzavi1718@gmail.com>
2022-10-29 19:16:10 +0530

    Auth re

--- script/auth.js
+++ script/auth.js
@@ -6,42 +6,26 @@ function signUp() {
     password = document.getElementById("pass").value;
     username = document.getElementById("username").value;
 
-    firebase.auth().useDeviceLanguage();
-    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign', {
-      'size': 'invisible',
-      'callback': (response) => {
-        // reCAPTCHA solved, allow signInWithPhoneNumber.
-        onSignInSubmit();
-      }
-    });
-    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
-const appVerifier = window.recaptchaVerifier;
-firebase.auth().signInWithPhoneNumber(email, appVerifier)
-    .then((confirmationResult) => {
-      // SMS sent. Prompt user to type the code from the message, then sign the
-      // user in with confirmationResult.confirm(code).
-      window.confirmationResult = confirmationResult;
-      // ...
-      console.log("success");
-    }).catch((error) => {
-      // Error; SMS not sent
-      console.log(error);
-      // ...
-    });
-}
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    document.getElementById("prompt").style.backgroundColor = "#4ca896";
+    document.getElementById("promptMessage").innerHTML = "Success";
+    document.getElementById("prompt").style.display = "block";
 
-function verify() {
-  const code = document.getElementById("pass").value;
-confirmationResult.confirm(code).then((result) => {
-  // User signed in successfully.
-  const user = result.user;
-  console.log("success");
-  // ...
-}).catch((error) => {
-  // User couldn't sign in (bad verification code?)
-  // ...
-  console.log(error);
-});
+    var user = userCredential.user;
+    return user.updateProfile({
+          displayName: username,
+        })
+        
+      }).catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    //ERROR TIP
+    document.getElementById("prompt").style.backgroundColor = "#d92324";
+    document.getElementById("promptMessage").innerHTML = "<i class='fas fa-exclamation-circle'></i> " + errorCode + " " + errorMessage;
+    document.getElementById("prompt").style.display = "block";
+  });
 }
 
 function signIn() {
