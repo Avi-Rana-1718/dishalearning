5b78bffa498a11825fecb8ac94217ca25d533b83
Avi Rana <itzavi1718@gmail.com>
2022-11-10 22:18:25 +0530

    Minor Changes

--- script/db.js
+++ script/db.js
@@ -1,11 +1,15 @@
+var uid;
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+
 document.getElementById("avg").innerHTML = "NaN";
 
 const dbRef = firebase.database().ref();
 
-dbRef.child("marks/uid").once('value').then((snapshot) => {
+dbRef.child("marks/" + user.uid).once('value').then((snapshot) => {
   if (snapshot.exists()) {
     var data = Object.values(snapshot.val());
-
     var total=0;
 
     for (i=0;i<data.length;i++) {
@@ -24,12 +28,18 @@ if (total==0) {
 }
 
   } else {
-    total=total + data[i].score;
     var ul = document.getElementById("list");
     var li = document.createElement("li");
     li.innerHTML = "No data found.";
     ul.appendChild(li)
   }
 }).catch((error) => {
-  console.error(error);
+  console.log(error);
+});
+
+
+} else {
+  // User is signed out
+  console.log("Signed Out!");
+}
 });
--- script/index.js
+++ script/index.js
@@ -1,10 +1,38 @@
 
-function dark() {
-    var fileref = document.createElement("link");
-fileref.rel = "stylesheet";
-fileref.type = "text/css";
-fileref.href = "style/theme-dark.css";
-document.getElementsByTagName("head")[0].appendChild(fileref);
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
 
-// document.getElementById("reviews").style.backgroundColor = "#1F2326";
+document.getElementById("avg").innerHTML = "NaN";
+
+const dbRef = firebase.database().ref();
+
+dbRef.child("marks/" + user.uid).once('value').then((snapshot) => {
+  if (snapshot.exists()) {
+    var data = Object.values(snapshot.val());
+    var total=0;
+
+    for (i=0;i<data.length;i++) {
+      total=total + data[i].score;
+      
+    }
+
+
+    document.getElementById("avg").innerHTML = total/data.length + "%";
+console.log("Data fetched!");
+if (total==0) {
+  document.getElementById("avg").innerHTML = "NaN"; 
 }
+
+  } else {
+    document.getElementById("avg").innerHTML = "NaN";
+  }
+}).catch((error) => {
+  console.log(error);
+});
+
+
+} else {
+  // User is signed out
+  console.log("Signed Out!");
+}
+});
