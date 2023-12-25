24d4ae48127725a87b84503c72833db616c3bf8d
Avi Rana <itzavi1718@gmail.com>
2023-12-26 00:20:12 +0530

    Update footer.js

--- react/footer.js
+++ react/footer.js
@@ -6,7 +6,9 @@ const root = ReactDOM.createRoot(
 <div>
 <a href="https://www.buymeacoffee.com/avirana">Pledge</a>
    <a href="support.html">Support</a>
+   <a href="https://www.termsfeed.com/live/065c588a-b68d-435a-b835-574664b9dac6">Privacy</a>
    <h5><img src="/images/logo.png" />@Disha Learning</h5>
+
 </div>
   );
 
