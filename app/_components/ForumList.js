b1456d5687c1929912c2309a205ffb37147288a2
Avi Rana <itzavi1718@gmail.com>
2024-07-27 00:51:12 +0530

    Update ForumList.js

--- app/_components/ForumList.js
+++ app/_components/ForumList.js
@@ -7,11 +7,11 @@ export default function ForumList(props) {
   return (
     <li className="list-none outline oultine-1 outline-2 outline-slate-400/25 p-3 rounded m-3">
       <Link href={link}>
-      <time className="text-xs text-[#6a6a6a]">{timestamp}</time>
+      <time className="text-xs text-[#21B77C]">{timestamp}</time>
       <h3 className="flex items-center hover:underline">{question}</h3>
       <ul className="flex">
         {tags.map((el, i) => {
-           return (<li className="text-xs mr-1" key={el + i}>{el}</li>);
+           return (<li className="text-xs mr-1 text-[#21B77C]" key={el + i}>{el}</li>);
         })}
       </ul>
       </Link>
