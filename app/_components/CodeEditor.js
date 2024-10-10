fc4c16f6bd939a88fbe65da8aa40d1fca2f67c23
Avi Rana <itzavi1718@gmail.com>
2024-10-10 23:41:25 +0530

    Update CodeEditor.js

--- app/_components/CodeEditor.js
+++ app/_components/CodeEditor.js
@@ -8,17 +8,7 @@ export default function CodeEditor(props) {
 
     return (
         <div className="">
-        <textarea
-        className="w-full outline oultine-1 outline-2 outline-slate-400/25 p-3 rounded"
-        rows={10}
-        onInput={(e)=>{
-            setData(e.target.value);
-        }}
-        ></textarea>
-        <div className="bg-[#e8e7e7] text-[#282828] p-3 my-6 rounded-lg">
-            <h3 className="text-lg underline">Output:</h3>
-            <span className="p-4" dangerouslySetInnerHTML={{__html: format(data)}}></span>
-        </div>
+
         </div>
     )
 }
