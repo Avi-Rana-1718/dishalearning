8a9958133fd8526bdee1e8833a844564280b398a
Avi Rana <itzavi1718@gmail.com>
2024-10-17 16:08:54 +0530

    Update forumList.js

--- app/_components/forumList.js
+++ app/_components/forumList.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { faQ } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import format from "@/format";
 
 export default function ForumList(props) {
   const { question, timestamp, url} = props;
@@ -10,7 +11,7 @@ export default function ForumList(props) {
     <li className="list-none outline outline-2 outline-slate-400/25 p-3 rounded m-3">
       <Link href={`/forums/${url}`}>
       <time className="text-xs text-[#6a6a6a]">{date}</time>
-      <h3 className="flex items-center hover:underline" dangerouslySetInnerHTML={{__html:question}}></h3>
+      <h3 className="flex items-center hover:underline" dangerouslySetInnerHTML={{__html:format(question)}}></h3>
       <ul className="flex">
         {/* {tags.map((el, i) => {
            return (<li className="text-xs mr-1 text-[#6a6a6a]" key={el + i}>{el}</li>);
