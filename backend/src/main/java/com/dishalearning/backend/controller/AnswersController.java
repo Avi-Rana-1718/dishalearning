dafa222fbad8e4750434fec317db2ba31ec7e156
Avi Rana <itzavi1718@gmail.com>
2025-06-14 20:22:58 +0530

    cors fixes

--- backend/src/main/java/com/dishalearning/backend/controller/AnswersController.java
+++ backend/src/main/java/com/dishalearning/backend/controller/AnswersController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dishalearning.backend.entity.AnswerEntity;
 import com.dishalearning.backend.service.AnswersService;
 
+@CrossOrigin(origins = "*")
 @RestController()
 @RequestMapping("/answers")
 public class AnswersController {
